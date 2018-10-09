8374c30bf14faffb987371a80edee0ad9c445428
allentsai93 <allentsai93@gmail.com>
2018-10-09 01:54:35 +0000

    commented out redux logger

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+//import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import { searchSubs, requestSubs } from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 
-const logger = createLogger();
+//const logger = createLogger();
 const rootReducer = combineReducers({ searchSubs, requestSubs })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
     <Provider store={store}>
